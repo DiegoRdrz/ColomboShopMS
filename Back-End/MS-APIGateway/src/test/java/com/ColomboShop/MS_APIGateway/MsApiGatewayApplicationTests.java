88bc6c0918a3662cf74f9732fa1002b6ed3a519c
DiegoRdrz <diegordrz455@gmail.com>
2024-06-29 15:59:00 -0500

    Configurando APIgateway y su enrutamiento

---
+++ Back-End/MS-APIGateway/src/test/java/com/ColomboShop/MS_APIGateway/MsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ColomboShop.MS_APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
